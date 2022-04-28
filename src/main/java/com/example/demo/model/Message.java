3ac5aa699c4de26ea158fc7b35c3730a9ce79801
caritoponce <lapaisacarito@gmail.com>
2022-04-28 08:59:58 -0500

    new project spring with kafka producer

---
+++ src/main/java/com/example/demo/model/Message.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Message {
+
+    private String author;
+    private String text;
+}
